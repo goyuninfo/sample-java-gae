9b914c71b7dc8a32013fd29e9441536a77d6470b
goyun.info@gmail.com <goyun.info@gmail.com@f8c35f67-4400-e266-d462-3592548a2006>
2011-05-12 03:51:16 +0000

    added CDI according to http://www.dzone.com/links/r/google_app_engine_143_jsf202fcs_weldcdi_111finalf.html

---
+++ src/info/goyun/sample/SampleGoyunInfoServlet.java
@@ -0,0 +1,13 @@
+package info.goyun.sample;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class SampleGoyunInfoServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
